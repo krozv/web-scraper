a87ce59cb9193f4a8a3af915783a88e023603164
krozv <krozv.code@gmail.com>
2024-03-17 19:00:52 +0900

    modify file

--- ch5-2.py
+++ ch5-2.py
@@ -1,15 +1,28 @@
-class Puppy:
-    
-    def __init__(self, name, breed) -> None:
+class Dog:
+    def __init__(self, name, age, breed) -> None:
         self.name = name
-        self.age = 0.1
+        self.age = age
         self.breed = breed
+
+class GuardDog(Dog):
+
+    def grrr_grrr(self):
+        print('Grrr Grrr!')
+
+class Puppy(Dog):
     
     def __str__(self) -> str:
         return f'Puppy name is {self.name}.'
+    
+    def woof_woof(self):
+        print('Woof Woof!')
 
-happy = Puppy('happy', 'beagle') # Puppy의 종류인 happy (instance 생성)
-choco = Puppy('choco', 'dalmatian')
 
-print(happy, choco)
+happy = Puppy('happy', 1, 'beagle') # Puppy의 종류인 happy (instance 생성)
+choco = Puppy('choco', 1, 'dalmatian')
+
+# print(happy, choco)
 # print(happy.name, happy.breed)
+happy.woof_woof()
+cute = GuardDog('cuty', 3, 'bulldog')
+cute.grrr_grrr()
